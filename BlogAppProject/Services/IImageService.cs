e1aa1816dc6b3b7548e21aa9c56f8a7cb7f62a5a
Rhaegar2012 <48417377+Rhaegar2012@users.noreply.github.com>
2021-11-01 07:03:37 -0700

    1112021-Update
    Solved issue with the new user registration, it was a problem caused by Avast Antivirus, which caused a conflict with the MailKit mail service  , it was removed by deactiving the anti virus shields.
    
    Still looking into the image display thing

--- BlogAppProject/Services/IImageService.cs
+++ BlogAppProject/Services/IImageService.cs
@@ -8,6 +8,7 @@ namespace BlogAppProject.Services
 {
      public interface IImageService
     {
+
         Task<byte[]> EncodeImageAsync(IFormFile file);
         Task<byte[]> EncodeImageAsync(string fileName);
         string DecodeImage(byte[] data, string type);
