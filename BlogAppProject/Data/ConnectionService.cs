54e8c2e1552a238109bf9161eb94d9134ebcb562
Rhaegar2012 <48417377+Rhaegar2012@users.noreply.github.com>
2021-11-08 06:56:01 -0800

    11082021-Update
    Added connection service for Heroku database integration

---
+++ BlogAppProject/Data/ConnectionService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogAppProject.Data
+{
+    public class ConnectionService
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var databaseURL = Environment.GetEnvironmentVariable("DATABASE_URL");
+            return string.IsNullOrEmpty(databaseURL) ? connectionString : BuildConnectionString(databaseURL);
+        }
+        private static string BuildConnectionString(string databaseUrl)
+        {
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split('.');
+            return new NpgsqlConnectionStringBuilder()
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Prefer,
+                TrustServerCertificate = true
+
+            }.ToString();
+
+        }
+    }
+}
